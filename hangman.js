0783eaed21cb25dfecb48e53085a0744a79e4ef1
balagang <bank139@hotmail.com>
2023-08-30 23:01:56 +0100

    add prototype method getPuzzle()

--- hangman.js
+++ hangman.js
@@ -1,10 +1,35 @@
+// Prototypal Inheritance
+
 const Hangman = function (word, remainingGuesses) {
-    this.word = word
+    // let characters = []
+    // console.log(word.length)
+    // for (let i = 0; i < word.length; i++) {
+    //     characters.push(word[i].toLowerCase())
+    // }
+    // console.log(characters)
+    // this.word = characters
+    this.word = word.toLowerCase().split('')
+    console.log(word.length)
     this.remainingGuesses = remainingGuesses
+    this.guessedLetters = ['s', 'c', 'a', 'h']
+}
+
+Hangman.prototype.getPuzzle = function (guessedLetters) {
+    let puzzle = ''
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+        } else {
+            puzzle += '*'
+        }
+    })
+    return puzzle
 }
 
 const game1 = new Hangman('Cat', 2)
-const game2 = new Hangman('Dog', 3)
+const game2 = new Hangman('Dog Shit', 3)
 
 console.log(game1)
+console.log(game1.getPuzzle('b'))
 console.log(game2)
+console.log(game2.getPuzzle())
