3f9b65b4c102f916b034f6f2e7ddf32dc339b2e2
balagang <bank139@hotmail.com>
2023-09-01 23:46:41 +0100

    add puzzle function

--- hangman.js
+++ hangman.js
@@ -1,35 +1,45 @@
 // Prototypal Inheritance
 
 const Hangman = function (word, remainingGuesses) {
-    // let characters = []
-    // console.log(word.length)
-    // for (let i = 0; i < word.length; i++) {
-    //     characters.push(word[i].toLowerCase())
-    // }
-    // console.log(characters)
-    // this.word = characters
     this.word = word.toLowerCase().split('')
-    console.log(word.length)
     this.remainingGuesses = remainingGuesses
-    this.guessedLetters = ['s', 'c', 'a', 'h']
+    this.guessedLetters = []
 }
 
 Hangman.prototype.getPuzzle = function (guessedLetters) {
     let puzzle = ''
-    this.word.forEach((letter) => {
-        if (this.guessedLetters.includes(letter) || letter === ' ') {
-            puzzle += letter
+    if (typeof guessedLetters === 'string') {
+        if (this.guessedLetters.includes(guessedLetters.toLowerCase())) {
+
+            this.word.forEach((letter) => {
+                if (this.guessedLetters.includes(letter) || letter === ' ') {
+                    puzzle += letter
+                } else {
+                    puzzle += '*'
+                }
+            })
+
+
         } else {
-            puzzle += '*'
+            this.guessedLetters.push(guessedLetters.toLowerCase())
+            this.word.includes(guessedLetters.toLowerCase()) ? {} : this.remainingGuesses = this.remainingGuesses - 1
+            this.word.forEach((letter) => {
+                if (this.guessedLetters.includes(letter) || letter === ' ') {
+                    puzzle += letter
+                } else {
+                    puzzle += '*'
+                }
+            })
         }
-    })
-    return puzzle
+
+
+        return puzzle
+    }
 }
+
 
 const game1 = new Hangman('Cat', 2)
 const game2 = new Hangman('Dog Shit', 3)
 
-console.log(game1)
-console.log(game1.getPuzzle('b'))
-console.log(game2)
-console.log(game2.getPuzzle())
+console.log(game1.getPuzzle('a'))
+console.log(game2.getPuzzle('s'))
