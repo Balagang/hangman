9806f1a3388c9796148599b44c394988548a858e
balagang <bank139@hotmail.com>
2023-09-02 00:07:28 +0100

    add prototype method makeGuess

--- hangman.js
+++ hangman.js
@@ -6,40 +6,37 @@ const Hangman = function (word, remainingGuesses) {
     this.guessedLetters = []
 }
 
-Hangman.prototype.getPuzzle = function (guessedLetters) {
+Hangman.prototype.getPuzzle = function () {
     let puzzle = ''
-    if (typeof guessedLetters === 'string') {
-        if (this.guessedLetters.includes(guessedLetters.toLowerCase())) {
-
-            this.word.forEach((letter) => {
-                if (this.guessedLetters.includes(letter) || letter === ' ') {
-                    puzzle += letter
-                } else {
-                    puzzle += '*'
-                }
-            })
-
-
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
         } else {
-            this.guessedLetters.push(guessedLetters.toLowerCase())
-            this.word.includes(guessedLetters.toLowerCase()) ? {} : this.remainingGuesses = this.remainingGuesses - 1
-            this.word.forEach((letter) => {
-                if (this.guessedLetters.includes(letter) || letter === ' ') {
-                    puzzle += letter
-                } else {
-                    puzzle += '*'
-                }
-            })
+            puzzle += '*'
         }
-
-
-        return puzzle
+    })
+    return puzzle
+}
+Hangman.prototype.makeGuess = function (guess) {
+    guess = guess.toLowerCase()
+    const isUnique = !this.guessedLetters.includes(guess)
+    const isBadGuess = !this.word.includes(guess)
+    if (typeof guess === 'string') {
+        if (isUnique) {
+            this.guessedLetters.push(guess)
+        } else if (isUnique && isBadGuess) {
+            this.remainingGuesses--
+        }
     }
 }
-
 
 const game1 = new Hangman('Cat', 2)
 const game2 = new Hangman('Dog Shit', 3)
 
-console.log(game1.getPuzzle('a'))
+console.log(game1.makeGuess('a'))
+console.log(game1.makeGuess('c'))
+console.log(game1.makeGuess('z'))
+console.log(game1.getPuzzle())
+console.log(game1.remainingGuesses)
+
 console.log(game2.getPuzzle('s'))
