7bf9e5b976bc3b502f99b165b9ba8f42aedff888
balagang <bank139@hotmail.com>
2023-09-16 17:48:13 +0100

    fetching data now using Promise in stead of callback function

--- app.js
+++ app.js
@@ -26,20 +26,37 @@ window.addEventListener('keypress', (e) => {
 })
 
 const wordCount = 2
-getPuzzle(wordCount, (error, puzzle) => {
-    if (error) {
-        console.log(error)
-    } else if (puzzle) {
-        console.log(puzzle)
-    }
+getPuzzle(wordCount).then((puzzle) => {
+    console.log(puzzle)
+}, (err) => {
+    console.log(err)
 })
 
 const countryCode = 'TH'
-getCountryDtetails(countryCode, (error, countryInfo) => {
-    if (error) {
-        console.log(error)
-    } else if (countryInfo) {
-        console.log(countryInfo)
-    }
+getCountryDtetails(countryCode).then((country) => {
+    console.log(country)
+}, (err) => {
+    console.log(err)
 })
-console.log('Do something else')
+
+// currying function using closure (Closure is the combination of a function and the lexical scope in which its was defined)
+const createTipper = (baseTip) => {
+    return (amount) => {
+        return `$${amount} bill, Tip ${baseTip * 100}% is $${amount * baseTip}`
+    }
+}
+
+const tip15Percent = createTipper(.15)
+const tip20Percent = createTipper(.20)
+console.log(tip15Percent(100))
+console.log(tip20Percent(350))
+
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Data from Resolve')
+        // reject('This is promise error')
+    }, 2000);
+})
+
+console.log(myPromise.then((returnData) => console.log(returnData)), (err) => console.log(err))
+// console.log(myPromise)
--- requeste.js
+++ requeste.js
@@ -1,5 +1,4 @@
-const getCountryDtetails = (countryCode, callback) => {
-
+const getCountryDtetails = (countryCode) => new Promise((resolve, reject) => {
     const countryRequest = new XMLHttpRequest()
     countryRequest.addEventListener('readystatechange', (e) => {
 
@@ -7,26 +6,40 @@ const getCountryDtetails = (countryCode, callback) => {
             const data = JSON.parse(e.target.responseText)
             // console.log(data)
             const country = data.find((element) => element.altSpellings[0] === countryCode)
-            callback(undefined, country.name.common)
+            resolve(country.name.common)
         } else if (e.target.readyState === 4) {
-            callback('Unable to fetch data', undefined)
+            reject('Unable to fetch data')
         }
     })
     countryRequest.open('GET', 'https://restcountries.com/v3.1/all')
     countryRequest.send()
+})
 
-}
+// const getPuzzle = (wordCount, callback) => {
+//     const request = new XMLHttpRequest()
+//     request.addEventListener('readystatechange', (e) => {
+//         if (e.target.readyState === 4 && e.target.status === 200) {
+//             const data = JSON.parse(e.target.responseText)
+//             callback(undefined, data.puzzle)
+//         } else if (e.target.readyState === 4) {
+//             callback('An error has taken place', undefined)
+//         }
+//     })
+//     request.open('GET', `https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+//     request.send()
+// }
 
-const getPuzzle = (wordCount, callback) => {
+const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
     const request = new XMLHttpRequest()
     request.addEventListener('readystatechange', (e) => {
         if (e.target.readyState === 4 && e.target.status === 200) {
             const data = JSON.parse(e.target.responseText)
-            callback(undefined, data.puzzle)
+            resolve(data.puzzle)
         } else if (e.target.readyState === 4) {
-            callback('An error has taken place', undefined)
+            reject('An error has taken place')
         }
     })
     request.open('GET', `https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
     request.send()
-}
+
+})
